bb8775523ac8f8a4ca5cc3fb3283bdd130414419
vollantre <rinbinary@gmail.com>
2020-01-16 20:52:51 -0400

    create category model

---
+++ models/category.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//Declare schema for Category
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 100
+    }
+  }
+)
+
+//Virtual for category's url
+CategorySchema
+  .virtual('url')
+  .get(() => '/inventory/category/' + this._id )
+
+//Exports model
+module.exports = mongoose.model('Category', CategorySchema)
